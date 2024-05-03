d8049b3d1083e34bb151f44063a145c92dd899a8
MohammedSadok <mohammed.sadok.2003@gmail.com>
2024-05-03 12:18:28 +0100

    update print monitoring day

--- components/print/PrintMonitoringDay.tsx
+++ components/print/PrintMonitoringDay.tsx
@@ -15,46 +15,40 @@ interface Props {
 const PrintMonitoringDay: React.FC<Props> = ({ monitoringDay }) => {
   return (
     <div className="space-y-3">
-      {Object.entries(monitoringDay).map(([localName, moduleData]) => (
-        <div key={localName}>
-          <Table className="border rounded-lg">
-            <TableHeader>
-              <TableRow>
-                <TableCell className="border text-center text-xs">
-                  Module
+      <Table className="border rounded-lg">
+        <TableHeader>
+          <TableRow>
+            <TableCell className="border text-center text-xs">Module</TableCell>
+            <TableCell className="border text-center text-xs">Locale</TableCell>
+            <TableCell className="border text-center text-xs">
+              Surveillance
+            </TableCell>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {Object.entries(monitoringDay).map(([localName, moduleData]) =>
+            moduleData.exams.map((exam, examIndex) => (
+              <TableRow key={examIndex}>
+                <TableCell className="border  text-xs">
+                  <p>S {examIndex + 1}:</p>
+                  <p>Module: {exam.examDetails.moduleName}</p>
+                  <p>
+                    {exam.examDetails.moduleResponsible?.firstName +
+                      " " +
+                      exam.examDetails.moduleResponsible?.lastName}
+                  </p>
                 </TableCell>
                 <TableCell className="border text-center text-xs">
-                  Locale
+                  {localName}
                 </TableCell>
                 <TableCell className="border text-center text-xs">
-                  Surveillance
+                  {exam.teachers.join(", ")}
                 </TableCell>
               </TableRow>
-            </TableHeader>
-            <TableBody>
-              {moduleData.exams.map((exam, examIndex) => (
-                <TableRow key={examIndex}>
-                  <TableCell className="border  text-xs">
-                    <p>S {examIndex + 1}:</p>
-                    <p>Module: {exam.examDetails.moduleName}</p>
-                    <p>
-                      {exam.examDetails.moduleResponsible?.firstName +
-                        " " +
-                        exam.examDetails.moduleResponsible?.lastName}
-                    </p>
-                  </TableCell>
-                  <TableCell className="border text-center text-xs">
-                    {localName}
-                  </TableCell>
-                  <TableCell className="border text-center text-xs">
-                    {exam.teachers.join(", ")}
-                  </TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </div>
-      ))}
+            ))
+          )}
+        </TableBody>
+      </Table>
     </div>
   );
 };
